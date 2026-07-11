e59e115855d3bef7821c26e16b4fed77155107c6
rgerges76 <rgergs94@gmail.com>
2026-07-11 16:29:52 -0500

    Update Supabase configuration values

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 window.MW_CONFIG = {
-  SUPABASE_URL: "https://fmp...supabase.co",
-  SUPABASE_ANON_KEY: "sb_publishable_المفتاح_الكامل",
+  SUPABASE_URL: "https://fmppmrckilezaptjupkh.supabase.co",
+  SUPABASE_ANON_KEY: "sb_publishable_-1bjCijQPK60H5K0UuKs3A_fh4ATNKG",
   TAX_RATE: 0.0925,
   STORE_NAME: "Mega Wireless Nashville",
   STORE_PHONE: "615-678-5849",
