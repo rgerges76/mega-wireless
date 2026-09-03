6a41df071b5814ca6cff2b7365db474e7525b998
rgerges76 <rgergs94@gmail.com>
2026-09-03 14:46:01 -0500

    Load bright marketing visual overrides

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import App from './App'
 import Hero3D from './Hero3D'
 import MarketingSections from './MarketingSections'
 import './index.css'
+import './marketing.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
