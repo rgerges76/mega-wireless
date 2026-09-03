3c93b32cb97c16acfb83531bbcb90100fe8de232
rgerges76 <rgergs94@gmail.com>
2026-09-03 14:43:25 -0500

    Mount trust and rewards marketing sections

--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import Hero3D from './Hero3D'
+import MarketingSections from './MarketingSections'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <Hero3D />
     <App />
+    <MarketingSections />
   </React.StrictMode>,
 )
